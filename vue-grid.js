470892e7022e128bb5e2fd8199c98bd1f9f5a252
frozenrunner <mike.horlick@gmail.com>
2017-01-08 13:51:16 -0500

    Moved code to component
    Moved the template into it's own compoenent and set up the binding to
    pass from input to the componenet.
    Set up header and calculated footer.
    Set footer to display only once data has been loaded.
    Set up basic input method to modify data

--- vue-grid.js
+++ vue-grid.js
@@ -1,9 +1,95 @@
-new Vue({
+Vue.component('vue-grid',{
+  template: `<table id="testGrid" class="table is-bordered is-striped">
+    <thead>
+      <tr>
+        <th v-for="header in tableHeader">
+          {{header}}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="tableRow in tableData">
+        <td @click="renderInput($event)" v-for="data in tableRow">
+          {{data}}
+        </td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td v-show="dataLoaded" v-for="foot in tableFooter">
+          {{foot}}
+        </td>
+      </tr>
+    </tfoot>
+  </table>`,
+  props: ['initCsvData'],
+  data() {
+      return {
+        highlightedRow: '',
+        dataLoaded: false,
+        activeInput: null,
+      }
+  },
+  methods: {
+    renderInput(event) {
+      if (this.activeInput == null){
+        var input = document.createElement("input");
+        var target = event.target;
+        input.value = event.target.textContent.trim();
+        event.target.textContent = '';
+        event.target.appendChild(input);
+        var vueComponent = this;
+        input.addEventListener("blur", function() {
+          var textNode = document.createTextNode(input.value)
+          target.removeChild(input);
+          target.appendChild(textNode);
+          vueComponent.activeInput = null;
+        })
+        this.activeInput = input
+      }
+    }
+  },
+  computed: {
+    tableHeader: function(){
+      return this.initCsvData[0];
+    },
+    tableData: function() {
+      var tempCsvData = this.initCsvData;
+      return tempCsvData.slice(1,51);
+    },
+    tableFooter: function() {
+      var footer = {};
+      var length = 0, width = 0, height = 0, price = 0;
+      for (var i = 1, len = this.initCsvData.length-1; i < len; i++){
+        length += parseFloat(this.initCsvData[i][3]);
+        width += parseFloat(this.initCsvData[i][4]);
+        height += parseFloat(this.initCsvData[i][5]);
+        price += parseFloat(this.initCsvData[i][6]);
+      }
+      footer[0] = 'Totals';
+      footer[1] = '';
+      footer[2] = '';
+      footer[3] = '$' + length.toFixed(2);
+      footer[4] = '$' + width.toFixed(2);
+      footer[5] = '$' + height.toFixed(2);
+      footer[6] = '$' + price.toFixed(2);
+      footer[7] = '';
+      footer[8] = '';
+      footer[9] = '';
+      footer[10] = '';
+      if (this.initCsvData.length > 0) {
+        this.dataLoaded = true
+      }
+      return footer;
+    }
+  }
+});
+
+var Test = new Vue({
   el: "#root",
   data: {
-    highlightedRow: '',
-    dataLoaded: false,
-    tableData: {}
+    showGrid: false,
+    csvTableData: ''
   },
   methods: {
     readFile(event) {
@@ -35,38 +121,18 @@ new Vue({
     jsonDataArray(array) {
       var headers = array[0].toString().split(',');
       var jsonData = [];
-      for ( var i = 1, length = array.length; i < length; i++ )
+      for ( var i = 0, length = array.length; i < length; i++ )
       {
         var row = array[i].toString().split(',');
         var data = {};
         for ( var x = 0; x < row.length; x++ )
         {
-          data[headers[x]] = row[x];
+          data[x] = row[x];
         }
         jsonData.push(data);
       }
-
-      this.tableData = {'page': '1', 'records': '100', 'rows': jsonData};
+      this.csvTableData = jsonData;
       this.dataLoaded = true;
-
-      $("#testGrid").jqGrid({
-    	datatype: "jsonstring",
-      datastr: this.tableData,
-      jsonReader: {repeatitems: false},
-    	height: 250,
-       	colNames:['Id','Description', 'Name', 'Length','Width','Height','Price', 'Territory', 'Class', 'Factor'],
-       	colModel:[
-       		{name:'Id',index:'Id', width:60, sortable:false},
-       		{name:'Description',index:'Description', width:90, sortable:false},
-       		{name:'Name',index:'Name', width:100, sortable:false},
-       		{name:'Length',index:'Length', width:80, align:"right", sortable:false},
-       		{name:'Width',index:'Width', width:80, align:"right", sortable:false},
-       		{name:'Height',index:'Height', width:80,align:"right", sortable:false},
-       		{name:'Price',index:'Price', width:150, sortable:false},
-       		{name:'Territory',index:'Territory', width:150, sortable:false},
-       		{name:'Class',index:'Class', width:150, sortable:false},
-       		{name:'Factor',index:'Factor', width:150, sortable:false},
-       	]});
     }
   }
 })
